c898971a6eb69566f9968298cee6a4f8b5006f66
Siddharth Chhoria <116880199+Dexterchhoria@users.noreply.github.com>
2025-07-25 14:22:36 +0530

    yo

--- franciium/backend-node/server.js
+++ franciium/backend-node/server.js
@@ -30,7 +30,4 @@ app.use('/api/cart', require('./routes/cart'));
 app.use('/api/orders', require('./routes/order'));
 app.use('/api', require('./routes/admin'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-}); 
+module.exports = app; 
